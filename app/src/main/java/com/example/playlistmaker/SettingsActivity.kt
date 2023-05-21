4f44625853d2bfb9c1b8d5b3a38bbcfbbbba8f57
AlexandrKorytin <al.corytin@yandex.ru>
2023-05-21 23:34:28 +0300

    спринт 9/18 тема 2/5 урок 3/3 В меню Settings реализованны нажатия на TextView - поделиться приложением - написать в поддержку - пользовательское соглашение

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -13,8 +16,35 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val back = findViewById<ImageView>(R.id.backSetting)
+        val writeToSupport = findViewById<TextView>(R.id.writeToSupport)
+        val shareApp = findViewById<TextView>(R.id.shareApp)
+        val userAgreement = findViewById<TextView>(R.id.userAgreement)
+
         back.setOnClickListener {
             finish()
         }
+
+        shareApp.setOnClickListener {
+            val shareAppIntent = Intent(Intent.ACTION_SEND)
+            shareAppIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_app_text))
+            shareAppIntent.setType("text/plain")
+            startActivity(shareAppIntent)
+        }
+
+        writeToSupport.setOnClickListener {
+            val message = getString(R.string.mail_support_massege)
+            val writeSupportIntent = Intent(Intent.ACTION_SENDTO)
+            writeSupportIntent.data = Uri.parse("mailto:")
+            writeSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("koritin@list.ru", "koritin84@gmail.com"))
+            writeSupportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mail_support_subject))
+            writeSupportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(writeSupportIntent)
+        }
+
+        userAgreement.setOnClickListener{
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_uri)))
+            startActivity(userAgreementIntent)
+        }
+
     }
 }
