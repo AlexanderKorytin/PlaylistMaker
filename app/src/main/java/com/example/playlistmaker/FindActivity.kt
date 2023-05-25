d8a48eae5a56ea19a182b986c43a244c4b4cbccf
AlexandrKorytin <al.corytin@yandex.ru>
2023-05-25 21:49:56 +0300

    спринт 9/18 тема 2/5 урок 3/3 1. В меню Settings в switch применен стиль для TextView с небольшим переформатирование верстки 2. Произведена верстка FindActivity

--- app/src/main/java/com/example/playlistmaker/FindActivity.kt
+++ app/src/main/java/com/example/playlistmaker/FindActivity.kt
@@ -3,6 +3,10 @@ package com.example.playlistmaker
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 
 class FindActivity : AppCompatActivity() {
@@ -10,9 +14,40 @@ class FindActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find)
+
         val back = findViewById<ImageView>(R.id.backFind)
+        val searchEditText = findViewById<EditText>(R.id.menuFind_SearchEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
         back.setOnClickListener {
             finish()
         }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+}
+
+private fun clearButtonVisibility(s: CharSequence?): Int {
+    return if (s.isNullOrEmpty()) {
+        View.GONE
+    } else {
+        View.VISIBLE
     }
 }
