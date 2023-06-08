ea9bf9974dbc6ee3e2583f63854716429b9f35c3
AlexandrKorytin <al.corytin@yandex.ru>
2023-06-09 00:30:22 +0300

    1. Сверстан элемент списка треков

---
+++ app/src/main/java/com/example/playlistmaker/FindViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.playlistmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class FindViewHolder(parent: View): RecyclerView.ViewHolder(parent) {
+
+}
