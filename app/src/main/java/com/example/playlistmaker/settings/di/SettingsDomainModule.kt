b3fd8eeeb202519cbbc80bea7764b224efeaf485
AlexandrKorytin <al.corytin@yandex.ru>
2023-09-25 18:57:16 +0300

    спринт 17 - удален single получения App

--- app/src/main/java/com/example/playlistmaker/settings/di/SettingsDomainModule.kt
+++ app/src/main/java/com/example/playlistmaker/settings/di/SettingsDomainModule.kt
@@ -5,13 +5,13 @@ import com.example.playlistmaker.settings.data.impl.SettingsRepositoryImpl
 import com.example.playlistmaker.settings.domain.api.SettingsInteractor
 import com.example.playlistmaker.settings.domain.api.SettingsRepository
 import com.example.playlistmaker.settings.domain.impl.SettingsInteractorImpl
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val settingsDomainModule = module {
-    single<App> { App() }
 
     factory<SettingsRepository> {
-        SettingsRepositoryImpl(switchPreferences = get(), app = get())
+        SettingsRepositoryImpl(switchPreferences = get(), app = androidApplication() as App)
     }
 
     factory<SettingsInteractor> {
