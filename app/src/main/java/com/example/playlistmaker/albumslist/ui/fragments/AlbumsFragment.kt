0bb1d03d542baaa1a12276ed65d78e7fbb289a28
AlexandrKorytin <al.corytin@yandex.ru>
2023-10-22 12:28:05 +0300

    Sprint 19 - удалены фрагменты плейсхолдером альбомов и избранных треков и сделаны часть франгментов ViewPager - tab_albums_fragment и tab_favorites_fragments обернуты в ScrollView

--- app/src/main/java/com/example/playlistmaker/albumslist/ui/fragments/AlbumsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/albumslist/ui/fragments/AlbumsFragment.kt
@@ -5,15 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import com.example.playlistmaker.R
 import com.example.playlistmaker.albumslist.ui.viewmodel.AlbumsListViewModel
-import com.example.playlistmaker.databinding.TabAlbumsFragmentContenerBinding
+import com.example.playlistmaker.databinding.TabAlbumsFragmentBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AlbumsFragment : Fragment() {
-    private var _binding: TabAlbumsFragmentContenerBinding? = null
+    private var _binding: TabAlbumsFragmentBinding? = null
     private val binding get() = _binding!!
     private val albumListVM: AlbumsListViewModel by viewModel<AlbumsListViewModel>()
 
@@ -26,18 +23,8 @@ class AlbumsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = TabAlbumsFragmentContenerBinding.inflate(inflater, container, false)
+        _binding = TabAlbumsFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        showPlaceholder()
-    }
-
-    private fun showPlaceholder() {
-        childFragmentManager.commit {
-            add<AlbumsFragmentPlaceholder>(R.id.tabAlbumsFragmentsContener)
-        }
-    }
 }
