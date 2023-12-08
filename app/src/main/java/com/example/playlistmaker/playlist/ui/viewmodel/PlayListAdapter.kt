f95163adb80f2eeae1125461928236921c21ccdf
AlexandrKorytin <al.corytin@yandex.ru>
2023-12-08 10:29:57 +0300

    спринт 22 - написан PlayListsDiffUtil.kt - написан PlayListAdapter.kt

---
+++ app/src/main/java/com/example/playlistmaker/playlist/ui/viewmodel/PlayListAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.playlistmaker.playlist.ui.viewmodel
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.example.playlistmaker.databinding.PlaylistItemBinding
+import com.example.playlistmaker.playlist.domain.models.PlayList
+import com.example.playlistmaker.playlist.ui.PlayListViewHolder
+
+class PlayListAdapter: ListAdapter<PlayList, PlayListViewHolder>(PlayListsDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayListViewHolder {
+       val viewBinding = PlaylistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlayListViewHolder(viewBinding)
+    }
+
+    override fun onBindViewHolder(holder: PlayListViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+}
